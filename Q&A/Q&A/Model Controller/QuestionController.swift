d771cf1bbb35bf453d609370a0672e6b0c488c82
Linh Bouniol <paulalinh89@gmail.com>
2018-07-30 13:26:25 -0700

    Created a model and model controller

---
+++ Q&A/Q&A/Model Controller/QuestionController.swift
@@ -0,0 +1,38 @@
+//
+//  QuestionController.swift
+//  Q&A
+//
+//  Created by Linh Bouniol on 7/30/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import Foundation
+
+class QuestionController {
+    
+    var questions: [Question] = []
+    
+    func createQuesiton(question: String, asker: String, answer: String?, answerer: String?) -> Question {
+        let question = Question(question: question, asker: asker, answer: answer, answerer: answerer)
+        questions.append(question)
+        
+        return question
+    }
+    
+    func update(question: Question, answer: String, answerer: String) {
+        
+        if let index = questions.index(of: question) {
+            var question = question
+            question.answer = answer
+            question.answerer = answerer
+            
+            questions.remove(at: index)
+            questions.insert(question, at: index)
+        }
+    }
+    
+    func delete(question: Question) {
+        guard let index = questions.index(of: question) else { return }
+        questions.remove(at: index)
+    }
+}
