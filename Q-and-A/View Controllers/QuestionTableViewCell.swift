54be7e6a5bdd73eef235081e792170c74e566c75
stdemicheli <stdemicheli@gmail.com>
2018-07-30 21:34:33 +0200

    Added QuestionTableView & Cell Storyboards, added QuestionTableViewController and QuestionTableViewCellController

---
+++ Q-and-A/View Controllers/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  Q-and-A
+//
+//  Created by De MicheliStefano on 30.07.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
