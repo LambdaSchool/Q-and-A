8a32c14edf1e7d73ae1acca834987fcb67f2ea81
stdemicheli <stdemicheli@gmail.com>
2018-07-30 21:19:24 +0200

    Added Question Model and QuestionController

---
+++ Q-and-A/Model Controllers/QuestionController.swift
@@ -0,0 +1,33 @@
+//
+//  QuestionController.swift
+//  Q-and-A
+//
+//  Created by De MicheliStefano on 30.07.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+import Foundation
+
+class QuestionController {
+    
+    func create(question: String, asker: String) {
+        let question = Question(question: question, asker: asker)
+        questions.append(question)
+    }
+    
+    func update(question: Question, answer: String, answerer: String) {
+        guard let index = questions.index(of: question) else { return }
+        var question = questions[index]
+        question.answer = answer
+        question.answer = answerer
+        questions.remove(at: index)
+        questions.insert(question, at: index)
+    }
+    
+    func delete(question: Question) {
+        guard let index = questions.index(of: question) else { return }
+        questions.remove(at: index)
+    }
+    
+    var questions: [Question] = []
+}
