2a604004442b9f5e1103577ccfe0ea9df7e1f757
stdemicheli <stdemicheli@gmail.com>
2018-07-30 23:57:08 +0200

    Added QuestionTableViewCell

--- Q-and-A/View Controllers/QuestionTableViewCell.swift
+++ Q-and-A/View Controllers/QuestionTableViewCell.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class QuestionTableViewCell: UITableViewCell {
 
+    // MARK: - Methods
+    
+    private func updateViews() {
+        guard let question = question else { return }
+        
+        questionTextLabel.text = question.question
+        askedByTextLabel.text = question.asker
+        if let _ = question.answer {
+            nextActionTextLabel.text = "Tap to view answer"
+        } else {
+            nextActionTextLabel.text = "Can you answer this?"
+        }
+    }
+    
     
     // MARK: - Properties
     
+    var question: Question? {
+        didSet {
+            updateViews()
+        }
+    }
     
     @IBOutlet weak var questionTextLabel: UILabel!
     @IBOutlet weak var askedByTextLabel: UILabel!
