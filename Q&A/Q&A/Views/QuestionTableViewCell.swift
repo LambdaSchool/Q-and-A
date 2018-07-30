9aa1bb530f4516509d1d7cd5b85a731d3fa70c0a
Linh Bouniol <paulalinh89@gmail.com>
2018-07-30 14:48:30 -0700

    Created a custom cell in a table view to display question

---
+++ Q&A/Q&A/Views/QuestionTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionTableViewCell.swift
+//  Q&A
+//
+//  Created by Linh Bouniol on 7/30/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var askedByLabel: UILabel!
+    @IBOutlet var statusLabel: UILabel!
+    
+}
