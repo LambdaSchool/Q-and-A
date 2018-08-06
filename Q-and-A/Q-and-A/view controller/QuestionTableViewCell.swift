f5ac6c759cceec6f9108cb660e06181ba9ca07e5
Iyin Raphael <iyinraphael@gmail.com>
2018-08-06 15:31:57 -0700

    added answerViewcontroller

---
+++ Q-and-A/Q-and-A/view controller/QuestionTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  QuestionTableViewCell.swift
+//  Q-and-A
+//
+//  Created by Iyin Raphael on 8/6/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var askerLabel: UILabel!
+}
