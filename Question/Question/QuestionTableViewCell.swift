a89a5b00fb817c8f3b67259e9423a2ae787cd52b
Farhan Farooqui <farhan@farooqui.net>
2018-09-03 18:52:10 -0400

    View Controllers created

---
+++ Question/Question/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  Question
+//
+//  Created by Farhan on 9/3/18.
+//  Copyright © 2018 Farhan. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
