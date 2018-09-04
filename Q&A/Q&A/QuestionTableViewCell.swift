87dcb11f2f4a1fcd8222ca1794eebe9e57d7b506
Moin Uddin <U_Moin75@yahoo.com>
2018-09-03 21:08:01 -0400

    Finish Q&A View Controller, working on Ask a Question View Controller

---
+++ Q&A/Q&A/QuestionTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionTableViewCell.swift
+//  Q&A
+//
+//  Created by Moin Uddin on 9/3/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var askerLabel: UILabel!
+    
+    
+    
+}
