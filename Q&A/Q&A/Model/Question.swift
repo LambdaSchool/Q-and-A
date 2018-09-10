56b0dabdd597d3b10e97950cee405341a555782d
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-09 23:30:50 -0700

    added files and organization

---
+++ Q&A/Q&A/Model/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  Q&A
+//
+//  Created by Welinkton on 9/9/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import Foundation
---
+++ Q&A/Q&A/View Controllers/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  Q&A
+//
+//  Created by Welinkton on 9/9/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
