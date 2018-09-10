c96b9e0f2c5d815a3532d95b42f9f5a26596dbeb
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-10 12:46:01 -0700

    updated Question controller - CRUD

--- Q&A/Q&A/Model/QuestionController.swift
+++ Q&A/Q&A/Model/QuestionController.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+class QuestionController {
+    
+    // CRUD
+    // Read + Create
+    
+    private(set) var questions: [Question] = []
+    
+    func createQuestion (question:String, asker:String) {
+        let question = Question(question: question, asker: asker)
+            questions.append(question)
+        }
+        
+    // UPDATE
+    // Update func that takes in answer string, and answerer string to add to the question
+    func updateQuestion(question: Question, answer:String, answerer: String){
+            guard let index = questions.index(of: question) else {return}
+        
+        //Declare new variable and set as new question
+        var newQuestion = question
+        newQuestion.answer = answer
+        newQuestion.answerer = answerer
+        questions.remove(at: index)
+        questions.insert(newQuestion, at: index)
+        
+        
+        // Trying to grab the initial question
+        //    questions[index].answer = answer
+        //    questions[index].answerer = answerer
+        
+    }
+    
+    // DELETE
+    
+    func deleteQuestion (question: Question) {
+        guard let index = questions.index(of: question) else {return}
+        questions.remove(at: index)
+    }
+    
+    
+    
+    
+    
+}
