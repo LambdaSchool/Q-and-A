bb603c3f6413633856dd0a7f1c31e23dd0a8dc39
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-11 15:08:09 -0700

    update TODO Labels

--- Q&A/Q&A/View Controllers/AnswerViewController.swift
+++ Q&A/Q&A/View Controllers/AnswerViewController.swift
@@ -12,10 +12,7 @@ class AnswerViewController: UIViewController {
 
     var questionConroller: QuestionController?
     
-    var question : Question? {
-        didSet { updateViews()
-        }
-    }
+    var question : Question?
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var askerLabel: UILabel!
@@ -34,20 +31,38 @@ class AnswerViewController: UIViewController {
     }
     
      func updateViews(){
-        if (question?.answer != nil && question?.answerer != nil) {
-            answererNameTextField?.text =  question?.answerer
-            answerTextView?.text = question?.answer
-        }
-        questionLabel?.text = question?.question
-        askerLabel?.text = question?.asker
-        self.title = question?.question
         
+        guard let question = question else { return }
+        
+        // TODO Fix This
+        title = question.question
+        questionLabel.text = question.question
+        askerLabel.text = question.asker
+        
+        guard let answerer = question.answerer,
+            let answer = question.answer else { return }
+        
+        answererNameTextField.text = answerer
+        answerTextView.text = answer
+        
+        
+//        if (question?.answer != nil && question?.answerer != nil) {
+//            answererNameTextField?.text =  question?.answerer
+//            answerTextView?.text = question?.answer
+//        }
+//
+//        questionLabel?.text = question?.question
+//        askerLabel?.text = question?.asker
+//        self.title = question?.question
+//
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateViews()
+        
         // Do any additional setup after loading the view.
     }
 
