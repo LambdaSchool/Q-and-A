edf1d4a3f6bda47dfb8576efc8637c9f5f3e9886
Paul Yi <yipaulc@gmail.com>
2018-10-10 15:41:36 -0700

    Clean up QuestionsTableViewController and add UITableViewDataSource methods

---
+++ Q&A/Q&A/QuestionsTableViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class QuestionsTableViewController: UITableViewController {
+
+   let questionController = QuestionController()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionController.questions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionCell", for: indexPath) as? QuestionTableViewCell else { return UITableViewCell() }
+        
+        let question = questionController.questions[indexPath.row]
+        cell.question = question
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let question = questionController.questions[indexPath.row]
+            questionController.delete(question: question)
+            
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    
+
+}
