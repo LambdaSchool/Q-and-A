26bc0738e6ffda421f1d52b500e6ef2c943cba3a
Sean Doyle <sdoylelambda@gmail.com>
2018-10-22 18:51:08 -0400

    storyboard and constraints

---
+++ Q&ARemake/Q&ARemake/Question.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct Question: Equatable {
+    let question:String
+    let asker:String
+    var answer:String?
+    var answerer:String?
+    
+    init(question: String, asker: String, answer: String? = nil , answerer: String? = nil) {
+        self.question = question
+        self.asker = asker
+        self.answer = answer
+        self.answerer = answerer
+    }
+}
