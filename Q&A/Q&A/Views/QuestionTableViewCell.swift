2bdab8061a74c394917b885a9bcd2fb892736638
Paul Yi <yipaulc@gmail.com>
2019-01-15 14:02:04 -0800

    Add storyboards and constraints

---
+++ Q&A/Q&A/Views/QuestionTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionTableViewCell.swift
+//  Q&A
+//
+//  Created by Paul Yi on 1/15/19.
+//  Copyright © 2019 Paul Yi. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var askerLabel: UILabel!
+    @IBOutlet weak var canYouAnswerLabel: UILabel!
+    
+}
