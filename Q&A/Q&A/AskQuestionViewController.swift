15334c65cd6b0f6f7b19ed55ff4cac857b1f2fd6
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-15 13:21:33 -0800

    Set up Answer UI and set connections to AnswerVC.

---
+++ Q&A/Q&A/AskQuestionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AskQuestionViewController.swift
+//  Q&A
+//
+//  Created by Nathanael Youngren on 1/15/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class AskQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func submitQuestion(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+
+}
