5c186ec1fdf8edc8b250615234dca4e196f41010
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-15 13:04:34 -0800

    Set up view controller to ask questions and connected it to associated view controller.

---
+++ Q&A/Q&A/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  Q&A
+//
+//  Created by Nathanael Youngren on 1/15/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
