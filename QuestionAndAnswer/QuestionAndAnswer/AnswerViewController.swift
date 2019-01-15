40b7973e7d0c5a76cf685823921e5e46308e8098
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-15 15:52:49 -0500

    Add answer view controller scene and stack view

---
+++ QuestionAndAnswer/QuestionAndAnswer/AnswerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AnswerViewController.swift
+//  QuestionAndAnswer
+//
+//  Created by Nelson Gonzalez on 1/15/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class AnswerViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
