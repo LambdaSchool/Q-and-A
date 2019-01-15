93713ab40dfc8e9338606587397bfda5cd09aee6
“Cameron <cameron.dunn2013@gmail.com>
2019-01-15 14:46:28 -0600

    Add Xcode project files and Question Struct

---
+++ Q and A/Q and A/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  Q and A
+//
+//  Created by Cameron Dunn on 1/15/19.
+//  Copyright © 2019 Cameron Dunn. All rights reserved.
+//
+
+import Foundation
+
+struct Question{
+    let question : String
+    let asker : String
+    var answer : String? = nil
+    var answerer : String? = nil
+    
+}
---
+++ Q and A/Q and A/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Q and A
+//
+//  Created by Cameron Dunn on 1/15/19.
+//  Copyright © 2019 Cameron Dunn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
