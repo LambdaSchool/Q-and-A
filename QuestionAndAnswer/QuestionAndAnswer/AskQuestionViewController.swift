b537698eadf78e79055b2d07c5f82cb8ab751510
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-15 15:41:41 -0500

    Add ask question scene and connect outlets/actions

---
+++ QuestionAndAnswer/QuestionAndAnswer/AskQuestionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AskQuestionViewController.swift
+//  QuestionAndAnswer
+//
+//  Created by Nelson Gonzalez on 1/15/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class AskQuestionViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var questionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func submitQuestionBarButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
