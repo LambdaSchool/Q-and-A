354aa2cf2ce4381ae9786c64f204c07b4068b005
Michael R.F <mrflowers22@uwalumni.com>
2019-01-15 18:48:46 -0500

    complete storyboard.

---
+++ QandA/QandA/QuestionsTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionsTableViewCell.swift
+//  QandA
+//
+//  Created by Michael Flowers on 1/15/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class QuestionsTableViewCell: UITableViewCell {
+    @IBOutlet weak var question: UILabel!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var askedByLabel: UILabel!
+    @IBOutlet weak var otherLabel: UILabel!
+    
+    
+}
