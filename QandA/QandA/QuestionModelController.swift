ea53272f0c10a430bde999e481eb3bbdfd35a718
Vijay Das <vijaytdas@gmail.com>
2019-01-23 14:04:08 -0600

    add deleteQuestion to QuestionModelController

--- QandA/QandA/QuestionModelController.swift
+++ QandA/QandA/QuestionModelController.swift
@@ -8,19 +8,31 @@
 
 import UIKit
 
-private(set) var questions: [Question] = []
+class QuestionModelController {
+
+
 
 
 // need to add create, update, and delete functions
+    
+    func createQuestion(question: String, asker: String) {
+        let question = Question(question: question, asker: asker)
+        questions.append(question)
+        
+    }
+    
+    func updateQuestion(question: Question, answer: String?, answerer: String?) {
+        guard let index = questions.index(of: question) else { return }
+        questions[index].answer = answer
+        questions[index].answerer = answerer
+        
+    }
+    
+    func deleteQuestion(question: Question) {
+        guard let index = questions.index(of: question) else { return }
+        questions.remove(at: index)
+        
+    }
 
-func createQuestion(question: String, asker: String) {
-    let question = Question(question: question, asker: asker)
-    questions.append(question)
+    private(set) var questions: [Question] = []
 }
-
-func updateQuestion(question: Question, answer: String?, answerer: String?) {
-    guard let index = questions.index(of: question) else { return }
-    questions[index].answer = answer
-    questions[index].answerer = answerer
-}
-
