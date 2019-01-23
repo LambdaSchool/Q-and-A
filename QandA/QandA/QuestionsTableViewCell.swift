ae4adb70d234b7a0fb53ac957abfbb06b32160b7
Vijay Das <vijaytdas@gmail.com>
2019-01-23 14:33:11 -0600

    add skeleton CocoaTouch files for QuestionsTableViewController and QuestionsTableViewCell

---
+++ QandA/QandA/QuestionsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionsTableViewCell.swift
+//  QandA
+//
+//  Created by Vijay Das on 1/23/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class QuestionsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
