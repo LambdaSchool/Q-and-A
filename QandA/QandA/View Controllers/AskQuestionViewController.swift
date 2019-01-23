0239fed04d5a1d8939b0d7300fce451c957659be
Vijay Das <vijaytdas@gmail.com>
2019-01-23 14:46:31 -0600

    add AskQuestion view and view controller and outlets and action

---
+++ QandA/QandA/View Controllers/AskQuestionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AskQuestionViewController.swift
+//  QandA
+//
+//  Created by Vijay Das on 1/23/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class AskQuestionViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var questionTextView: UITextView!
+    
+
+    @IBAction func questionSubmitButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
