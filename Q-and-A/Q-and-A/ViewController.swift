17bc4f5491a72f3332a329b14a0556d09621f7cf
Christopher Aronson <christopheraronson@icloud.com>
2019-04-25 11:48:12 -0700

    Initial commit

---
+++ Q-and-A/Q-and-A/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Q-and-A
+//
+//  Created by Christopher Aronson on 4/25/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
