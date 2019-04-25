e91a36e7300d0fb5e8c961c3d36a94cec44ff26b
Christopher Aronson <christopheraronson@icloud.com>
2019-04-25 13:43:46 -0700

    Created QuestionsTableView storyboard and prototype cell. Created QuestionTableViewController and QuestionTableViewCell classes and made IB connections.

---
+++ Q-and-A/Q-and-A/QuestionTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  Q-and-A
+//
+//  Created by Christopher Aronson on 4/25/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var askedByLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
