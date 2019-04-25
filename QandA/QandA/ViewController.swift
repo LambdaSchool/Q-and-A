20b5e0c5402e29f1f0af2c00fef41c061e3b48df
Jeffrey Carpenter <spaz926@me.com>
2019-04-25 13:12:37 -0500

    Create Initial Xcode Project

---
+++ QandA/QandA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QandA
+//
+//  Created by Jeffrey Carpenter on 4/25/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
