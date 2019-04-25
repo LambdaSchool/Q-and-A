0acdb0d2253c48e1fe55abbf5417575506decae2
Michael Redig <mredig@gmail.com>
2019-04-25 16:22:27 -0500

    set up ui and connected outlets

---
+++ Q and A/Q and A/QuestionCreationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  QuestionCreationViewController.swift
+//  Q and A
+//
+//  Created by Michael Redig on 4/25/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+import UIKit
+
+class QuestionCreationViewController: UIViewController {
+
+	@IBOutlet var nameTextField: UITextField!
+	@IBOutlet var questionTextView: UITextView!
+	
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+	@IBAction func submitQuestionButtonPressed(_ sender: UIBarButtonItem) {
+	}
+}
