2c36c7e0cedb21da7f09d61c561342f71838b0ca
Christopher Aronson <christopheraronson@icloud.com>
2019-04-25 11:57:05 -0700

    Created the Question struct

---
+++ Q-and-A/Q-and-A/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  Q-and-A
+//
+//  Created by Christopher Aronson on 4/25/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let question: String
+    let asker: String
+    var answer: String?
+    var answerer: String?
+}
