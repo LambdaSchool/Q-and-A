e3a3a7ea009880776c7cbccc0ee7461fab141050
Jeffrey Carpenter <spaz926@me.com>
2019-04-25 14:46:03 -0500

    Added TableView, Custom Cell, and Ask Question UI
    Added custom Question Cell
    Added custom Question View Controller

---
+++ QandA/QandA/QuestionTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionTableViewCell.swift
+//  QandA
+//
+//  Created by Jeffrey Carpenter on 4/25/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var askerLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+}
