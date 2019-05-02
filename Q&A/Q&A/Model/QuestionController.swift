8eaa551d7cbc5f25127dc02c7c13065baed5fb4a
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-02 14:39:14 -0600

    Added Create, Update, Delete functions to QuestionController

---
+++ Q&A/Q&A/Model/QuestionController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionController.swift
+//  Q&A
+//
+//  Created by Hayden Hastings on 5/2/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import Foundation
+
+class QuestionController {
+    
+    func deleteQuestion(for question: String, asker: String, answer: String, answerer: String) {
+        let deleteQuestion = Question(question: question, asker: asker, answer: answer, answerer: answerer)
+        questions.append(deleteQuestion)
+    }
+    
+    func updateQuestion(for question: String, asker: String, answer: String, answerer: String) {
+        let updateQuestion = Question(question: question, asker: asker, answer: answer, answerer: answerer)
+        questions.append(updateQuestion)
+    }
+    
+    func createQuestion(for question: String, asker: String) {
+        let question = Question(question: question, asker: asker)
+        questions.append(question)
+    }
+    
+    var questions: [Question] = []
+}
