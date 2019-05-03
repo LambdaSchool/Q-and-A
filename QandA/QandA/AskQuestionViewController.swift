d63ab68cefc6bac88e9de37dbd7c501c5e796fc2
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-05-03 11:23:54 -0400

    Ask Question View Controller, text field, text view, button, segue set-up

---
+++ QandA/QandA/AskQuestionViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AskQuestionViewController.swift
+//  QandA
+//
+//  Created by Nathan Hedgeman on 5/3/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import UIKit
+
+class AskQuestionViewController: UIViewController {
+
+    
+    @IBOutlet weak var NameTextField: UITextField!
+    
+    @IBOutlet weak var QuestionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func SubmitButtonTapped(_ sender: Any) {
+    }
+    
+  
+}
