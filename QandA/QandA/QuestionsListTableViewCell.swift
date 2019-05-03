dbc1cbe27126a393daff46bcd27d0e6fe730934b
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-05-02 20:54:00 -0400

    Added Questions List Table View Cell/Controller.swift

---
+++ QandA/QandA/QuestionsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionsListTableViewCell.swift
+//  QandA
+//
+//  Created by Nathan Hedgeman on 5/2/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import UIKit
+
+class QuestionsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
