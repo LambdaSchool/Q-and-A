c09c2e85140921b1ec5b3e885104bee191cb4ee0
shillwil <alex.shillingford@icloud.com>
2019-06-06 15:48:26 -0600

    Part 2 up to creating and linking AskQuestionViewController complete

---
+++ Q-and-A/Q-and-A/QuestionTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionTableViewCell.swift
+//  Q-and-A
+//
+//  Created by Alex Shillingford on 6/6/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var questionTextLabel: UILabel!
+    @IBOutlet weak var askedByLabel: UILabel!
+    
+}
